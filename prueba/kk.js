fe937f0fefa8eafede33d9e24fe1e83cedfe5d65
unknown <gt104515@gmail.com>
2022-12-30 15:30:54 +0100

    redes sociales footer

--- prueba/kk.js
+++ prueba/kk.js
@@ -3,8 +3,38 @@ let flechaLeft=document.getElementById("polla");
 flechaLeft.addEventListener("click",function (){
     alert("kff");
 });
-*/
+
 
 function pene(){
     alert("horn");
 }
+index=0;
+function añadir(){
+    let tallasContainer=document.getElementById("tallas");
+    let divTalla=document.createElement("div");
+    divTalla.className = "talla" +index;
+    divTalla.innerHTML=index;
+    index++;
+    tallasContainer.appendChild(divTalla)
+    
+}
+
+function actualizar(){
+    let tallasContainer=document.getElementById("tallas");
+
+    let talla1=document.getElementsByClassName("talla1");
+    let talla2=document.getElementsByClassName("talla2");
+
+    tallasContainer.replaceChild(talla1,talla2)
+}
+
+function eliminar(){
+    let tallasContainer=document.getElementById("tallas");
+    
+    let talla1=document.getElementsByClassName("talla1");
+
+    tallasContainer.appendChild(talla1)
+
+    tallasContainer.removeChild(talla1);
+}
+*/
